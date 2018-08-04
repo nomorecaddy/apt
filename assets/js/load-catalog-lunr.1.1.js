2485df42768b0df4b017d14733bb76e5409d16e0
nomorecaddy <nomorecaddy@yahoo.com>
2018-08-04 14:10:04 +0000

    auto update from iTest community git commit build 17

--- assets/js/load-catalog-lunr.1.1.js
+++ assets/js/load-catalog-lunr.1.1.js
@@ -79,6 +79,7 @@ $(document).ready(function() {
                             '<i class="fa fa-2x"></i><span>'+catalog[ref].class+'</span>'+  
                         '</div>'+  
                         '<div class="pull-right user-icons">'+               
+                            '<a href='+catalog[ref].hrefReadme+'><i class="fa fa-file-text fa-2x"></i></a>'+  
                             '<a href='+catalog[ref].link+'><i class="fa fa-cloud-download fa-2x"></i></a>'+  
                         '</div>'+  
                     '</div>'+  
